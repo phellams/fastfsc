65a2e73fd08f2a7c420a2a16f2bce9368436cd3f
sgkens <sgkens@gmail.com>
2025-08-22 01:31:55 +1000

    👷 cicd: Update gitlab config
    🧳 Notes:
    
      - UPDATE: nupsforge to bypass shelldock for ci pipline runes with -ci flag

--- devops/scripts/deploy/deploy-gitlab.ps1
+++ devops/scripts/deploy/deploy-gitlab.ps1
@@ -17,21 +17,23 @@ else { $ModuleVersion = "$ModuleVersion-$prerelease" }
 try {
   Write-host -foregroundcolor yellow "Attempting to Register Gitlab: $gitlab_uri@$Gitlab_Username"
   #dotnet nuget add source $gitlab_uri/$NugetProjectPath --name gitlab --username $GitLab_Username --password $ENV:GITLAB_API_KEY
-  nuget sources add -name "gitlab_$projectid_$ModuleName`_Packages" -source $gitlab_uri/$NugetProjectPath -username $GitLab_Username -password $env:GITLAB_PUBLIC_API_KEY
+  nuget sources add -name "gitlab_$projectid_$ModuleName`_Packages" -source $gitlab_uri/$NugetProjectPath -username $GitLab_Username -password $env:GITLAB_API_KEY
   Write-host -foregroundcolor green "Complete"
 }
 catch [system.exception] {
   Write-Host "Failed to push to gitlab"
   Write-Host $_
+  exit 1
 }
 
 try {
   Write-host -foregroundcolor yellow "Attempting to push $modulename to Gitlab: $gitlab_uri/$NugetProjectPath"
   #dotnet nuget push ./dist/nuget/$modulename.$SemVerVersion.nupkg --source gitlab 
-  nuget push ./dist/nuget/$ModuleName.$ModuleVersion.nupkg -Source "gitlab_$projectid_$ModuleName`_Packages" -ApiKey $env:GITLAB_PUBLIC_API_KEY
+  nuget push ./dist/nuget/$ModuleName.$ModuleVersion.nupkg -Source "gitlab_$projectid_$ModuleName`_Packages" -ApiKey $env:GITLAB_API_KEY
   nuget sources remove -Name "gitlab_$projectid_$ModuleName`_Packages"
 }
 catch [system.exception] {
   Write-Host "Failed to push to gitlab"
   Write-Host $_
+  exit 1
 }
