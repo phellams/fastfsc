c0bd02afdf693ba52dd3a64b3f43cfd5620437ca
sgkens <sgkens@gmail.com>
2025-09-12 03:40:07 +1000

    👷 cicd(devops): Update gitlab workflow
    🧰 Build: patch
    
    🧳 Notes:
    
      - UPDATE: psgallary deploy script to check if version exists before attempting to
        deploy
      - UPDATE: build script to create choco package env file for gitlab ci step
      - FIX: choco nuspec creation to use .net path concatenation for module source path
      - FIX: psgallary deploy script typo in variable name '' to ''

--- devops/scripts/deploy/deploy-psgallary.ps1
+++ devops/scripts/deploy/deploy-psgallary.ps1
@@ -12,12 +12,13 @@ else { $ModuleVersion = "$ModuleVersion-$prerelease" }
 
 
 # Check if module version exists
-$psgal_curretnversion = Find-Module -Name $modulename `
-                                    -RequiredVersion $ModuleVersion `
-                                    -Repository 'psgallery' `
-                                    -AllowPrerelease | Select-Object -ExpandProperty Version
-                                    
-if ($psgal_curretnversion -eq $ModuleVersion) {
+[console]::writeline("Checking if module version $ModuleVersion exists in PSGallery")
+[string]$psgal_curretnversion = Find-Module -Name $modulename `
+                                            -RequiredVersion $ModuleVersion `
+                                            -Repository 'psgallery' `
+                                            -AllowPrerelease | Select-Object -ExpandProperty Version
+
+if ($psgal_currentnversion -eq $ModuleVersion) {
   [console]::writeline("Module version $ModuleVersion already exists in PSGallery")
   exit 0
 } else {
