c3c1512217281269abbdd67924ecc109d3c6d141
sgkens <sgkens@gmail.com>
2025-09-13 06:32:53 +1000

    👷 cicd(build-scripts): Update deploy-artifacts ps1 script

--- devops/scripts/deploy/deploy-artifacts.ps1
+++ devops/scripts/deploy/deploy-artifacts.ps1
@@ -1,9 +1,9 @@
 #---CONFIG----------------------------
-$ModuleConfig    = Get-Content -Path ./build_config.json | ConvertFrom-Json
-$ModuleName      = $ModuleConfig.moduleName
-$ModuleManifest  = Test-ModuleManifest -path "./dist/$ModuleName/$ModuleName.psd1"
+$ModuleConfig            = Get-Content -Path ./build_config.json | ConvertFrom-Json
+$ModuleName              = $ModuleConfig.moduleName
+$ModuleManifest          = Test-ModuleManifest -path "./dist/$ModuleName/$ModuleName.psd1"
 [string]$moduleversion   = $ModuleManifest.Version.ToString()
-$PreRelease      = $ModuleManifest.PrivateData.PSData.Prerelease
+$PreRelease              = $ModuleManifest.PrivateData.PSData.Prerelease
 
 #---CONFIG----------------------------
 if (!$prerelease -or $prerelease.Length -eq 0) { $ModuleVersion = $ModuleVersion }
@@ -13,8 +13,9 @@ $headers = @{ "JOB-TOKEN" = $env:CI_JOB_TOKEN }
 $baseUrl = "$env:CI_API_V4_URL/projects/$env:CI_PROJECT_ID/packages/generic/module/$env:CI_COMMIT_TAG"
 
 # Upload NuGet package
-$nugetFile = Get-ChildItem -Recurse ./dist/nuget -Filter "$modulename-$ModuleVersion.nupkg" | Select-Object -First 1
-if ($nugetFile) {
+[console]::writeline("Finding NuGet package: $ModuleName-$ModuleVersion to upload...")
+$nugetFile = Get-ChildItem -Recurse './dist/nuget' -Filter "$ModuleName-$ModuleVersion.nupkg" | Select-Object -First 1
+if ($null -ne $nugetFile) {
     [console]::writeline("Uploading NuGet package: $($nugetFile.Name)")
     Invoke-RestMethod -Uri "$baseUrl/$($nugetFile.Name)" -Method Put -InFile $nugetFile.FullName -Headers $headers
     [console]::writeline("Uploaded: $($nugetFile.Name)")
@@ -24,8 +25,9 @@ if ($nugetFile) {
 }
 
 # Upload Chocolatey package
-$chocoFile = Get-ChildItem -Recurse ./dist/choco -Filter "$modulename-$ModuleVersion-choco.nupkg" | Select-Object -First 1
-if ($chocoFile) {
+[console]::writeline("Finding Chocolatey package: $ModuleName-$ModuleVersion-choco.nupkg to upload...")
+$chocoFile = Get-ChildItem -Recurse ./dist/choco -Filter "$ModuleName-$ModuleVersion-choco.nupkg" | Select-Object -First 1
+if ($null -ne $chocoFile) {
     [console]::writeline("Uploading Chocolatey package: $($chocoFile.Name)")
     Invoke-RestMethod -Uri "$baseUrl/$($chocoFile.Name)" -Method Put -InFile $chocoFile.FullName -Headers $headers
     [console]::writeline("Uploaded: $($chocoFile.Name)")
@@ -35,8 +37,9 @@ if ($chocoFile) {
 }
 
 # Upload ZIP file
-$zipFile = Get-ChildItem -Recurse ./dist/psgal -Filter "$modulename-$ModuleVersion-psgal.zip" | Select-Object -First 1
-if ($zipFile) {
+[console]::writeline("Finding ZIP file: $ModuleName-$ModuleVersion-psgal.zip to upload...")
+$zipFile = Get-ChildItem -Recurse ./dist/psgal -Filter "$ModuleName-$ModuleVersion-psgal.zip" | Select-Object -First 1
+if ($null -ne $zipFile) {
     [console]::writeline("Uploading ZIP file: $($zipFile.Name)")
     Invoke-RestMethod -Uri "$baseUrl/$($zipFile.Name)" -Method Put -InFile $zipFile.FullName -Headers $headers
     [console]::writeline("Uploaded: $($zipFile.Name)")
